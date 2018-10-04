ff6a2472bdbe8b85b04893b99ec772038140b9e2
burtyang <1105332245@qq.com>
2018-10-04 12:35:11 +0800

    增加https访问方式

--- fengkong.js
+++ fengkong.js
@@ -1,3 +1,4 @@
+const fs=require('fs');
 const express=require('express');
 const expressSession=require('express-session');
 const compression=require('compression');  //开启gzip压缩
@@ -6,7 +7,7 @@ const cookieParser=require('cookie-parser');
 const consolidate=require('consolidate');
 const session=require('connect-session');
 const MysqlStore=require('connect-mysql')(expressSession);
-const fs=require('fs');
+const enforceHttps = require('express-sslify'); //强制使用https
 const path=require('path');
 const { pool } = require('./backstage/db.js');
 const options = {
@@ -15,27 +16,34 @@ const options = {
 }
 
 //创建中间件服务器
-const server=express();
+var privateKey  = fs.readFileSync('./2_burtyang.top.key', 'utf8');
+var certificate = fs.readFileSync('./1_burtyang.top_bundle.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+const app=express();
+const server=require('https').createServer(credentials, app);
+//转换http 请求https
+app.use(enforceHttps.HTTPS({redirectMethods:['GET']}));
 
+//const server=require('http').createServer(app);
 server.listen(8001,(err)=>{
     if(err)
         throw new err;
     else 
         console.log('成功监听8001端口。');
 });
-server.use(compression());
+app.use(compression());
 //获取数据请求  post方式
-server.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
     extended:true,
     maxAge:5*1024*1024,
     limit: '50mb'
 }));
-server.use(cookieParser('fsafgs4324rfrt34edfg5'));//签名
+app.use(cookieParser('fsafgs4324rfrt34edfg5'));//签名
 (function(){
     var sessionArr=[];
     for(var i=0;i<100000;i++)
         sessionArr.push('key_'+Math.random().toString().replace('.',''));
-        server.use(expressSession({
+        app.use(expressSession({
         secret:'fsafgs4324rfrt34edfg5',
         name:'my_session_id',
         cookie:{maxAge:24*3600*1000},
@@ -67,14 +75,14 @@ setInterval(sessionCleanup, 1800000);
 
 //3.模版
 //输出什么东西
-server.set('view engine','html');
+app.set('view engine','html');
 //模版文件放在哪儿
-server.set('views','./web/dist');
+app.set('views','./web/dist');
 //哪种模版引擎
-server.engine('html',consolidate.ejs);
+app.engine('html',consolidate.ejs);
 
 //route
-server.use('/api',require('./backstage/router/web/index.js')());
+app.use('/api',require('./backstage/router/web/index.js')());
 
 //静态文件放置位置，即根目录
-server.use(express.static('./web/dist'));
+app.use(express.static('./web/dist'));
